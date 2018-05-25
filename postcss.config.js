ba13bb8b85290926169d32e694725a5495a70474
Sophie Orta <orta.sophie@gmail.com>
2018-05-25 18:32:09 -0400

    PostCSS setup

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+/* eslint global-require: 0, import/no-extraneous-dependencies: 0 */
+
 module.exports = {
+  map: { inline: false },
   plugins: [
+    require('postcss-easy-import')({ extensions: '.pcss' }),
+    require('postcss-preset-env')({
+      stage: 2,
+      features: ['custom-media-queries', 'nesting-rules'],
+    }),
     require('autoprefixer')({ }),
-    require('postcss-preset-env')({ }),
+    require('cssnano')({ preset: 'default' }),
   ],
 };
