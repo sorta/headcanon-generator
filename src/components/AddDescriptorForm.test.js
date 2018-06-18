b038028487208cd5a2caa6312110863ff694be17
Sophie Orta <orta.sophie@gmail.com>
2018-06-17 21:45:57 -0400

    Updated AddDescriptorForm test

--- src/components/AddDescriptorForm.test.js
+++ src/components/AddDescriptorForm.test.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import AddDescriptorForm from './AddDescriptorForm';
 
 const props = {
   addDescriptor: jest.fn(),
-  isManaging: () => true,
-  isNotManaging: () => false,
+  isManaging: () => false,
 };
-const component = (
-  <AddDescriptorForm addDescriptor={props.addDescriptor} isManaging={props.isNotManaging} />
-);
-const managingComponent = (
-  <AddDescriptorForm addDescriptor={props.addDescriptor} isManaging={props.isManaging} />
-);
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  shallow(component, div);
+// Tests for regular mode
+describe("AddDescriptorForm", () => {
+  const component = shallow(<AddDescriptorForm addDescriptor={props.addDescriptor} isManaging={props.isManaging} />);
+
+  it('renders and matches our snapshot', () => {
+    const snapshot = renderer.create(
+      <AddDescriptorForm addDescriptor={props.addDescriptor} isManaging={props.isManaging} />
+    );
+    const tree = snapshot.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should be null when not managing', () => {
+    expect(component.getElement()).toBe(null);
+  });
 });
 
-it('managing mode renders without crashing', () => {
-  const div = document.createElement('div');
-  shallow(managingComponent, div);
+
+// Tests for managing mode
+props.isManaging = () => true;
+describe("AddDescriptorForm in managing mode", () => {
+  // const component = shallow(<AddDescriptorForm addDescriptor={props.addDescriptor} isManaging={props.isManaging} />);
+
+  it('renders and matches our snapshot', () => {
+    const snapshot = renderer.create(
+      <AddDescriptorForm addDescriptor={props.addDescriptor} isManaging={props.isManaging} />
+    );
+    const tree = snapshot.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should have a name input field', () => {
+    const component = shallow(<AddDescriptorForm addDescriptor={props.addDescriptor} isManaging={props.isManaging} />);
+    expect(component.find('input[name="name"]').exists()).toBe(true);
+  });
+
+  it('should have a submit button', () => {
+    const component = shallow(<AddDescriptorForm addDescriptor={props.addDescriptor} isManaging={props.isManaging} />);
+    expect(component.find('button[type="submit"]').exists()).toBe(true);
+  });
+
+  it('form submit should call addDescriptor', () => {
+    const component = mount(<AddDescriptorForm addDescriptor={props.addDescriptor} isManaging={props.isManaging} />);
+    component.find('input[name="name"]').instance().value = 'TESTING';
+    component.find('button[type="submit"]').simulate('submit');
+    expect(props.addDescriptor.mock.calls.length).toEqual(1);
+  });
 });
